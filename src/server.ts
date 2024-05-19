b1247ec3ab8bba7a445346e9547333d87ca4e695
Nitto <nittosantos.dev@gmail.com>
2024-05-18 22:39:30 -0300

    feat: :sparkles: add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { app } from "./app";
 import { env } from "./env";
 
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT,
-}).then(() => {
-  console.log('🚀 Rodou samerda 😊')
-})
+app
+  .listen({
+    host: "0.0.0.0",
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log("🚀 Rodou samerda 😊");
+  });
