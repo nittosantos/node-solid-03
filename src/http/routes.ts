17592cb90bbec4ad6afff34f2c56a47cd04cc239
Nitto <nittosantos.dev@gmail.com>
2024-07-12 22:29:57 -0300

    feat: :sparkles:

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -6,5 +6,7 @@ import { register } from './controllers/register'
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
   app.get('/me', profile)
 }
