966ebf64da14e91cc19d5dd39eaadab9980e52b3
Nitto <nittosantos.dev@gmail.com>
2024-07-13 15:01:27 -0300

    feat: :sparkles:

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
-import { appRoutes } from './http/routes'
 import { ZodError } from 'zod'
 import { env } from '@/env'
-import fastifyJwt from '@fastify/jwt'
+import { usersRoutes } from '@/http/controllers/users/routes'
+import { gymsRoutes } from '@/http/controllers/gyms/routes'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
@@ -10,7 +12,9 @@ app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 })
 
-app.register(appRoutes)
+app.register(usersRoutes)
+app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
@@ -18,12 +22,10 @@ app.setErrorHandler((error, _, reply) => {
       .status(400)
       .send({ message: 'Validation error.', issues: error.format() })
   }
-
   if (env.NODE_ENV !== 'production') {
     console.error(error)
   } else {
     // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
   }
-
   return reply.status(500).send({ message: 'Internal server error.' })
 })
